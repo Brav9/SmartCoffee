869fa214042cf3aeeb0b5d03e716c30e0ee1355b
Brav9 <vskh93.009@gmail.com>
2022-10-09 16:42:29 +0300

    Переход из StartFragment к CakeFragment при нажатии на кнопку.

--- app/src/main/java/com/hfad/smartcoffee/StartFragment.kt
+++ app/src/main/java/com/hfad/smartcoffee/StartFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.hfad.smartcoffee.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
+
+    private var binding: FragmentStartBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +22,18 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
 
     fun orderProduct() {
         findNavController().navigate(R.id.action_startFragment2_to_cakeFragment)
+
     }
 }
