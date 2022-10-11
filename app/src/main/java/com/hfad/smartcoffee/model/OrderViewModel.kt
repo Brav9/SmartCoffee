b3978f67d41e0faffb94e358c831837af675f116
Brav9 <vskh93.009@gmail.com>
2022-10-11 21:27:57 +0300

    Создание параметров класса OrderViewModel.kt и метода getPickupOptions().

--- app/src/main/java/com/hfad/smartcoffee/model/OrderViewModel.kt
+++ app/src/main/java/com/hfad/smartcoffee/model/OrderViewModel.kt
@@ -1,8 +1,35 @@
 package com.hfad.smartcoffee.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
+class OrderViewModel : ViewModel() {
 
-class OrderViewModel: ViewModel() {
+    private val _product = MutableLiveData<String>()
+    val product: LiveData<String> = _product
 
+    private val _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> = _quantity
+
+    private val _date = MutableLiveData<String>()
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<Double> = _price
+
+    val dateOptions = getPickupOptions()
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(3) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
 }
