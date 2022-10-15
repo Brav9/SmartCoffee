22ee2f4979e427220b86340b857d186e8cdeb2ae
Brav9 <vskh93.009@gmail.com>
2022-10-15 21:43:28 +0300

    Добавление TextView стоимость заказа в fragment_cake.xml

--- app/src/main/java/com/hfad/smartcoffee/CakeFragment.kt
+++ app/src/main/java/com/hfad/smartcoffee/CakeFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.hfad.smartcoffee.databinding.FragmentCakeBinding
 import com.hfad.smartcoffee.databinding.FragmentStartBinding
+import com.hfad.smartcoffee.model.OrderViewModel
 
 
 class CakeFragment : Fragment() {
+
     private var binding: FragmentCakeBinding? = null
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
