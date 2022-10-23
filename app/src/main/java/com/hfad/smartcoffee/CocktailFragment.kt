c02c889028da91085f169e6af672e72dec80d4f5
Brav9 <vskh93.009@gmail.com>
2022-10-23 22:10:52 +0300

    Добавление sharedViewModel в CocktailFragment.kt

--- app/src/main/java/com/hfad/smartcoffee/CocktailFragment.kt
+++ app/src/main/java/com/hfad/smartcoffee/CocktailFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.hfad.smartcoffee.databinding.FragmentCakeBinding
 import com.hfad.smartcoffee.databinding.FragmentCocktailBinding
+import com.hfad.smartcoffee.model.OrderViewModel
 
 class CocktailFragment : Fragment() {
 
     private var binding: FragmentCocktailBinding? = null
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
